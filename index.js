6e7de7c61aba61692a90ce40356332bcadccf235
thomps0189 <angelathompson2011@gmail.com>
2021-12-16 15:59:07 -0600

    generate html file

---
+++ index.js
@@ -0,0 +1,43 @@
+// Packages needed for this app
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// TODO: Create an array of questions for employee info
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?"
+    },
+    {
+        type: "checkbox",
+        name: "role",
+        message: "What is the employee's role?",
+        choices: [
+            {name: "Manager"},
+            {name: "Engineer"},
+            {name: "Intern"}
+        ]
+    }, 
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the employee's email?"
+    },
+    {
+        type: "input",
+        name: "notes",
+        message: "Please include any additional notes (ex. number, GitHub, school, etc.)."
+    }
+]
+
+// TODO: Create a function to write the HTML file
+
+// TODO: Create function to create the file. 
+
+// Function call to create page
