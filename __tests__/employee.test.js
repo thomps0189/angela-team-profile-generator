d9fee8d7b3017eed336ea3d63c0dc982702c61bc
thomps0189 <angelathompson2011@gmail.com>
2021-12-19 12:23:40 -0600

    employee tests complete

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -7,3 +7,19 @@ test("creates employee object", () => {
    expect(employee.id).toBe("123");
    expect(employee.email).toBe("test@test.com");
 });
+
+test("get employee name", () => {
+    const employee = new Employee("Jim", "123", "test@test.com");
+
+    expect(employee.getName()).toEqual(expect.any(String));
+})
+
+test("get employee id number", () => {
+    const employee = new Employee("Jim", 123, "test@test.com");
+    expect(employee.getId()).toEqual(expect.any(Number))
+});
+
+test("get employee email", () => {
+    const employee = new Employee("Jim", 123, "test@test.com");
+    expect(employee.getEmail()).toEqual(expect.any(String));
+})
